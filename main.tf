bbe4fa895f8c1bc86b12cea53492f8daadbc3ba1
kealeyg <66959582+kealeyg@users.noreply.github.com>
2021-03-30 16:36:09 -0400

    updates

--- main.tf
+++ main.tf
@@ -3,3 +3,15 @@
 variable "globals" {}
 provider "azurerm" { alias = "sub" }
 data "azurerm_client_config" "current" {provider = azurerm.sub}
+
+locals {
+  prefix = "${var.globals.env}-${var.globals.group}"
+  unique      = substr(sha1(azurerm_resource_group.rg.id), 0, 3)
+  storageName = lower(join("", [var.globals.env, "asccm", local.unique, "stg"]))
+}
+
+/* Output
+------------------------------------------------------------------*/
+output "moduleOutput" {
+    value = local.storageName
+}
