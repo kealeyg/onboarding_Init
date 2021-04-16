3b1490d06faf5d30aa9806706647796345d23ab8
kealeyg <66959582+kealeyg@users.noreply.github.com>
2021-04-16 10:43:40 -0400

    updates

--- rg.tf
+++ rg.tf
@@ -1,13 +1,13 @@
 /* Create RG
 ------------------------------------------------------------------*/
 resource "azurerm_resource_group" "keyvault-rg" {
-    name = join("", [var.globals.env,"-",var.globals.group,"-",var.globals.project,"_","Keyvault","-","rg"])
+    name = join("", [var.globals.env,"-",var.globals.group,"-",var.globals.project,"-","Keyvault","-","rg"])
     location = "canadaCentral"
     provider = azurerm.sub
     tags = var.globals.tags
 }
 resource "azurerm_resource_group" "storage-rg" {
-    name = join("", [var.globals.env,"-",var.globals.group,"-",var.globals.project,"_","Storage","-","rg"])
+    name = join("", [var.globals.env,"-",var.globals.group,"-",var.globals.project,"-","Storage","-","rg"])
     location = "canadaCentral"
     provider = azurerm.sub
     tags = var.globals.tags
